469a15e2af34141ae69943ea257a789e66bb3c94
thisisnadya <nadyamumtazah33@gmail.com>
2022-10-20 20:24:26 +0700

    finishing post input

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -5,8 +5,22 @@ exports.registerUser = (req, res) => {
       res.status(406).json({ err: "You have to fill the registration form" });
       return;
     }
-
     let { email, username, password, passwordCheck } = req.body;
+
+    if (!email || !password || !passwordCheck) {
+      return res.status(406).json({ err: "Not all field have been entered" });
+    }
+
+    if (password.length < 8) {
+      return res
+        .status(406)
+        .json({ err: "Password must be at least 8 characters long" });
+    }
+
+    if (password !== passwordCheck) {
+      res.status(406).json({ err: "Password not match" });
+    }
+
     res.json({ email, username, password, passwordCheck });
   } catch (error) {
     res.status(500).json({ err: error.message || "Error while registration" });
