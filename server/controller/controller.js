fe22bc2bce998696da06c357f8ec007db96ee82c
thisisnadya <nadyamumtazah33@gmail.com>
2022-10-25 17:23:28 +0700

    save register user & authenticate the user

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcrypt");
+const User = require("../model/schema");
 
 exports.registerUser = async (req, res) => {
   try {
@@ -26,13 +27,29 @@ exports.registerUser = async (req, res) => {
     // hashing password
     const hash = await bcrypt.hashSync(password, 10);
 
-    res.json({ email, username, hash, passwordCheck });
+    // using document structure
+    const newUser = new User({
+      email,
+      password: hash,
+      username,
+    });
+
+    newUser
+      .save(newUser)
+      .then((register) => {
+        res.json(register);
+      })
+      .catch((error) =>
+        res.status(406).json({
+          err: error.message || "Something went wrong while registration",
+        })
+      );
   } catch (error) {
     res.status(500).json({ err: error.message || "Error while registration" });
   }
 };
 
-exports.login = (req, res) => {
+exports.login = async (req, res) => {
   try {
     // validate req
     if (!req.body) {
@@ -48,11 +65,17 @@ exports.login = (req, res) => {
       res.status(406).json({ err: "Not all fields have been entered" });
     }
 
-    const user = "$2b$10$5ZkuiruVx2wXba70NgSAbe.CywpLKlszO5O3yYDw.taZ.3PMRC97.";
-    // compare the password
-    const isMatch = bcrypt.compare(password, user);
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(406).json({ err: "No account with this email" });
+    }
 
-    res.json({ email, isMatch });
+    // compare the password
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) return res.status(406).json({ err: "Invalid Credentials" });
+
+    res.json({ username: user.username, email: user.email });
   } catch (error) {
     res.status(500).json({ err: error.message || "Error while login" });
   }
---
+++ server/model/schema.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
+
+module.exports = User = mongoose.model("user", userSchema);
